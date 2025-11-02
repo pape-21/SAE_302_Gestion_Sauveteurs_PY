55f0862bbccb9bc0c232dd357b4122ab7db6ed88
etudiant <etudiant@I4-virtu>
2025-11-02 21:45:19 +0100

    debut model crud

---
+++ Gestion_Sauveteur_Speleologue.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""
+Application graphique de gestion des sauveteurs spéléologues
+Chef de projet : <Ton nom>
+Date : 2025-11-02
+"""
+
+from gestion_sauveteurs.gestion_sauveteurs import main as run_app
+
+if __name__ == "__main__":
+    print("🚀 Lancement de l'application de gestion des sauveteurs spéléologues...")
+    run_app()
