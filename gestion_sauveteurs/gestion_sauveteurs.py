fafe30318c64487ab0151354e6cb24d42697f514
pape <fallpapebirame3@gmail.com>
2025-11-21 11:34:22 +0100

    Mise à jour du script principal gestion_sauveteurs

--- gestion_sauveteurs/gestion_sauveteurs.py
+++ gestion_sauveteurs/gestion_sauveteurs.py
@@ -5,22 +5,8 @@ from pathlib import Path
 import json
 from gestion_sauveteurs.connexion_réseaux import pair, charger_ips_machines
 
-# ----------------------------------------------------
-#  INITIALISATION BASE DE DONNÉES
-# ----------------------------------------------------
-db_path = Path("data/database.db")
-conn = sqlite3.connect(db_path)
-cur = conn.cursor()
 
-if db_path.stat().st_size == 0:
-    with open("data/script_base_de_donne.sql", "r", encoding="utf-8") as f:
-        sql_script = f.read()
-    cur.executescript(sql_script)  
-    conn.commit()
-    print("Base de données initialisée.")
 
-conn.close()
-print("Application prête à fonctionner.")
 
 #  CONFIGURATION RÉSEAU
 liste_machines = charger_ips_machines()
