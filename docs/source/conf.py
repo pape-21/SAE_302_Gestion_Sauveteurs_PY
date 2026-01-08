8b83f328ff3cbfbbcb579542543a107019bc238d
pape <fallpapebirame3@gmail.com>
2026-01-08 02:54:14 +0100

    documentation sphinx

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -1,33 +1,40 @@
 # Configuration file for the Sphinx documentation builder.
-#
-# For the full list of built-in configuration values, see the documentation:
-# https://www.sphinx-doc.org/en/master/usage/configuration.html
+import os
+import sys
+
+# --- MODIFICATION MAJEURE ICI ---
+# On dit à Sphinx : "Remonte de 2 dossiers pour trouver le code source"
+# Si conf.py est dans docs/source/, ../.. nous amène à la racine du projet.
+sys.path.insert(0, os.path.abspath('../..'))
+# --------------------------------
 
 # -- Project information -----------------------------------------------------
-# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
-
 project = 'Application graphique de gestion des sauveteurs spéléologues'
 copyright = '2025, Pape Birame Fall, Mohamed Fall, Perceval Behanzin, Sokhna Gueye'
 author = 'Pape Birame Fall, Mohamed Fall, Perceval Behanzin, Sokhna Gueye'
 release = '0.1'
 
 # -- General configuration ---------------------------------------------------
-# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = ['sphinx.ext.todo',
-'sphinx.ext.mathjax',
-'sphinx.ext.ifconfig',
-'sphinx.ext.autodoc',
-'sphinx.ext.viewcode',
+extensions = [
+    'sphinx.ext.todo',
+    'sphinx.ext.mathjax',
+    'sphinx.ext.ifconfig',
+    'sphinx.ext.autodoc',   # Lit le code
+    'sphinx.ext.viewcode',  # Ajoute des liens vers le code source
+    'sphinx.ext.napoleon'   # <--- INDISPENSABLE : Pour le style Google (Args/Returns)
 ]
+
+# Configuration de Napoleon (pour que ça soit joli)
+napoleon_google_docstring = True
+napoleon_numpy_docstring = False
+napoleon_include_init_with_doc = True
+napoleon_include_private_with_doc = False
 
 templates_path = ['_templates']
 exclude_patterns = []
 
-
-
 # -- Options for HTML output -------------------------------------------------
-# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'alabaster'
+html_theme = 'sphinx_rtd_theme'
 html_static_path = ['_static']
--- docs/source/index.rst
+++ docs/source/index.rst
@@ -3,26 +3,48 @@
    You can adapt this file completely to your liking, but it should at least
    contain the root `toctree` directive.
 
-Welcome to Application graphique de gestion des sauveteurs spéléologues's documentation!
-========================================================================================
+.. Documentation Gestion Sauveteurs
 
-Documentation du code
-*********************
+Bienvenue dans la documentation de Gestion Sauveteurs !
+=======================================================
+
+Cette documentation détaille le fonctionnement technique de l'application, 
+notamment les interactions avec la base de données (CRUD).
 
 .. toctree::
    :maxdepth: 2
-   :caption: Contents:
+   :caption: Table des matières:
+
+Module CRUD (Base de données)
+-----------------------------
+
+Cette partie gère toutes les communications avec la base de données SQLite.
+
+Gestion des Sauveteurs
+~~~~~~~~~~~~~~~~~~~~~~
+.. automodule:: gestion_sauveteurs.crud.sauveteur
+   :members:
+   :undoc-members:
+   :show-inheritance:
+
+Gestion du Planning
+~~~~~~~~~~~~~~~~~~~
+.. automodule:: gestion_sauveteurs.crud.planning
+   :members:
+   :undoc-members:
+   :show-inheritance:
+
+Gestion des Utilisateurs
+~~~~~~~~~~~~~~~~~~~~~~~~
+.. automodule:: gestion_sauveteurs.crud.utilisateur
+   :members:
+   :undoc-members:
+   :show-inheritance:
 
 
-
-Indices and tables
-==================
+Indices et Tables
+-----------------
 
 * :ref:`genindex`
 * :ref:`modindex`
 * :ref:`search`
-
-
-
-
-
